1f057396f6f7a09f33aabf0101bc51e210764021
James <james.gong927@gmail.com>
2018-02-24 10:01:46 +1100

    Add the TripPlannerBDD test with cucumber and serenity framework

---
+++ src/test/java/com/transportnswtesting/testcases/TripPlannerBDDTest.java
@@ -0,0 +1,35 @@
+package com.transportnswtesting.testcases;
+
+import com.transportnswtesting.steps.TripPlannerSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+/**
+ Created by daddyspro on 23/2/18.
+ */
+
+@RunWith(SerenityRunner.class)
+public class TripPlannerBDDTest {
+
+    @Managed
+    WebDriver driver;
+
+    @Steps
+    TripPlannerSteps plannerSteps;
+
+    @Test
+    public void canListTrips() {
+        // GIVEN
+        plannerSteps.given_someone_is_planning_the_trip();
+
+        // WHEN
+//        plannerSteps.when_he_excutes_a_trip_from_A_to_B("North Sydney Station", "Town Hall Station");
+
+        // THEN
+//        plannerSteps.then_a_list_of_trips_should_be_provided();
+    }
+}
